634ee3374b86922715291a58018d4074b806e8e1
boringmachine <machine449@gmail.com>
2013-03-31 11:56:19 +0900

    update source format

--- getImage.php
+++ getImage.php
@@ -1,19 +1,19 @@
 <?php
-if(array_key_exists("name", $_GET)){
-  //open json file
-  $name = $_GET["name"];
-  $filename = 'data.json';
-  $handle = fopen($filename, 'r');
-  $data = json_decode(fread($handle, filesize($filename)),TRUE);
-  if(array_key_exists($name,$data)){
-    $data[$name]++;
-  }else{
-    $data[$name]=1;
-  }
-  fclose($handle);
-  $handle = fopen($filename,'w');
-  fwrite($handle,json_encode($data));
-  fclose($handle);
+if (array_key_exists("name", $_GET)) {
+	//open json file
+	$name = $_GET["name"];
+	$filename = 'data.json';
+	$handle = fopen($filename, 'r');
+	$data = json_decode(fread($handle, filesize($filename)), TRUE);
+	if (array_key_exists($name, $data)) {
+		$data[$name]++;
+	} else {
+		$data[$name] = 1;
+	}
+	fclose($handle);
+	$handle = fopen($filename, 'w');
+	fwrite($handle, json_encode($data));
+	fclose($handle);
 }
 
 //init
@@ -22,34 +22,30 @@ $file_array = array();
 $handle = opendir(IMG_DIR) or die('Couldn\'t open.');
 // get file at IMG_DIR
 while ($fname = readdir($handle)) {
-  if (is_file(IMG_DIR . $fname)) {
-    array_push($file_array, $fname);
-  }
-} 
+	if (is_file(IMG_DIR . $fname)) {
+		array_push($file_array, $fname);
+	}
+}
 
 //finalize
 closedir($handle);
 
 //init
-$max = count($file_array)-1;
+$max = count($file_array) - 1;
 $min = 0;
-$key1 = rand($min,$max);
-while(($key2 = rand($min,$max)) == $key1);
+$key1 = rand($min, $max);
+while (($key2 = rand($min, $max)) == $key1);
 
 // set image file
 $img1 = $file_array[$key1];
 $img2 = $file_array[$key2];
 
 // echo file name as json
-$id_name = array("img1","img2");
-echo json_encode(
-array(
-      $id_name[0]=>$img1,
-      $id_name[1]=>$img2
-));
+$id_name = array("img1", "img2");
+echo json_encode(array($id_name[0] => $img1, $id_name[1] => $img2));
 
-/* debug 
-foreach ($file_array as $key=>$value)
-   echo $key." ".$value . '<br />';
-*/
+/* debug
+ foreach ($file_array as $key=>$value)
+ echo $key." ".$value . '<br />';
+ */
 ?>
--- getRanking.js
+++ getRanking.js
@@ -1,13 +1,17 @@
-$(function(){
-    var url = "data.json";
-    $.getJSON(url,function(data){
-	$.each(data,function(key,value){
-	    var $img = $("<img/>").attr({src:key,width:150,height:150});
-	    var $td1 = $("<td>").append($img);
-	    var $td2 = $("<td>").append(value);
-	    var $tr  = $("<tr>").append($td1);
-	    $tr.append($td2);
-	    $("tbody").append($tr);
+$(function() {
+	var url = "data.json";
+	$.getJSON(url, function(data) {
+		$.each(data, function(key, value) {
+			var $img = $("<img/>").attr({
+				src : key,
+				width : 150,
+				height : 150
+			});
+			var $td1 = $("<td>").append($img);
+			var $td2 = $("<td>").append(value);
+			var $tr = $("<tr>").append($td1);
+			$tr.append($td2);
+			$("tbody").append($tr);
+		});
 	});
-    });
 });
