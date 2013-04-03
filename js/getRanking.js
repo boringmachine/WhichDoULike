70eaa185a0879c09fcafe08a288ca3976b1589c5
boringmachine <machine449@gmail.com>
2013-04-03 14:05:43 +0900

    update

--- js/getRanking.js
+++ js/getRanking.js
@@ -1,17 +1,17 @@
 $(function() {
-    var url = "data.json";
-    $.getJSON(url, function(data) {
-	$.each(data, function(key, value) {
-	    var $img = $("<img/>").attr({
-		src : key,
-		width : 150,
-		height : 150
-	    });
-	    var $td1 = $("<td>").append($img);
-	    var $td2 = $("<td>").append(value);
-	    var $tr = $("<tr>").append($td1);
-	    $tr.append($td2);
-	    $("tbody").append($tr);
+	var url = "data.json";
+	$.getJSON(url, function(data) {
+		$.each(data, function(key, value) {
+			var $img = $("<img/>").attr({
+				src : key,
+				width : 150,
+				height : 150
+			});
+			var $td1 = $("<td>").append($img);
+			var $td2 = $("<td>").append(value);
+			var $tr = $("<tr>").append($td1);
+			$tr.append($td2);
+			$("tbody").append($tr);
+		});
 	});
-    });
 });
